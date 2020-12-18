327d8fb34bded0174bae1692aec251f207a308fc
giridhar7632 <giridhar.talla2002@gmail.com>
2020-12-18 14:17:37 +0530

    messages

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,23 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const { Client } = require('discord.js')
-const client = new Client();
+const client = new Client()
+const prefix="$"
+const def="ist"
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in`)
+})
+
+client.on("message", (message) => {
+  if(message.author.bot) return
+  if(message.content.startsWith(prefix)){
+    const cmd = message.content
+      .trim(4)
+      .substring(prefix.length)
+      .split(/\s+/)
+    console.log(cmd)
+  }
+})
 
 client.login(process.env.DISCORD_BOT_TOKEN);
